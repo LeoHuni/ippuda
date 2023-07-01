bc42d95e2f80b28538c7f37eb3e1b78ce413a8fe
LeoHuni <68592195+LeoHuni@users.noreply.github.com>
2023-07-01 13:03:42 +0900

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -61,7 +61,7 @@ count = 0
 
 
 def mode_select(num):
-    mode = [ 'About App' , '가상 성형 AI' , '피부 진단 AI', '병원 정보' ]
+    mode = [ '✨ About App' , '💉 가상 성형 AI' , '👧 피부 진단 AI', '🏥 병원 정보' ]
     app = st.sidebar.selectbox('choose the Appmode' ,
                                     mode ,
                                     key=str(num)
